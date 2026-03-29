50379ebbcb3a91d6a8715dd0930f282088843063
BarkinKctp <barkinkocatepe12@gmail.com>
2026-03-29 21:55:03 +0300

    Update Terraform workflows: modify backend configuration and adjust Terraform commands

--- main.tf
+++ main.tf
@@ -8,7 +8,13 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "terraform-backend-bucket-aws-26"
+    key            = "terraform-project/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
 
 module "app_stack" {
