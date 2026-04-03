46be2ed73ddff96bcef0fe1f98099a0f33d3d5da
BarkinKctp <barkinkocatepe12@gmail.com>
2026-04-03 23:05:40 +0300

    Update Terraform deployment workflow: add lock timeout to plan and apply steps; enhance outputs with state bucket and DynamoDB table ARNs

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -8,6 +8,16 @@ output "dynamodb_table_name" {
   value       = aws_dynamodb_table.terraform_locks.name
 }
 
+output "state_bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.terraform_state.arn
+}
+
+output "dynamodb_table_arn" {
+  description = "ARN of the DynamoDB table"
+  value       = aws_dynamodb_table.terraform_locks.arn
+}
+
 output "backend_init_command" {
   value = <<-EOT
     terraform init \
